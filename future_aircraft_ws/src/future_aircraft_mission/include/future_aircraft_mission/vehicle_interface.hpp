222ec9350887287fd5eba8a930c943d8e70e89a0
root <root@PC-202307281902.localdomain>
2026-09-03 13:26:09 +0800

    state init

--- future_aircraft_ws/src/future_aircraft_mission/include/future_aircraft_mission/vehicle_interface.hpp
+++ future_aircraft_ws/src/future_aircraft_mission/include/future_aircraft_mission/vehicle_interface.hpp
@@ -1,2 +1,24 @@
 #pragma once
 #include <ros/ros.h>
+#include <string>
+#include <mavros_msgs/State.h>
+
+class VehicleInterface
+{
+public:
+    VehicleInterface(ros::NodeHandle& nh, ros::NodeHandle& pnh);
+
+    bool isConnected() const;
+    bool isArmed() const;
+    std::string mode() const;
+
+private:
+    void stateCallback(const mavros_msgs::State::ConstPtr& msg);
+
+
+    ros::Subscriber _state_sub;
+
+    mavros_msgs::State _state;
+
+    std::string _state_topic;
+};
--- future_aircraft_ws/src/future_aircraft_mission/src/vehicle_interface.cpp
+++ future_aircraft_ws/src/future_aircraft_mission/src/vehicle_interface.cpp
@@ -1,2 +1,25 @@
 #include "future_aircraft_mission/vehicle_interface.hpp"
 
+VehicleInterface::VehicleInterface(
+    ros::NodeHandle& nh,
+    ros::NodeHandle& pnh)
+{
+    pnh.param<std::string>(
+        "state_topic",
+        _state_topic,
+        "/uav1/mavros/state"
+    );
+
+    _state_sub = nh.subscribe(
+        _state_topic,
+        10,
+        &VehicleInterface::stateCallback,
+        this
+    );
+}
+
+void VehicleInterface::stateCallback(
+    const mavros_msgs::State::ConstPtr& msg)
+{
+    _state = *msg;
+}
