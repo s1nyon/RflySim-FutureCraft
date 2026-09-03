f343254da41df2ce56165f087a10f8e733173dbd
root <root@PC-202307281902.localdomain>
2026-09-03 19:29:55 +0800

    odom

--- future_aircraft_ws/src/future_aircraft_mission/src/vehicle_interface.cpp
+++ future_aircraft_ws/src/future_aircraft_mission/src/vehicle_interface.cpp
@@ -3,18 +3,30 @@
 VehicleInterface::VehicleInterface(
     ros::NodeHandle& nh,
     ros::NodeHandle& pnh)
-    : _has_state(false)
+    : _has_state(false),
+      _has_odom(false)
 {
     pnh.param<std::string>(
         "state_topic",
         _state_topic,
         "/uav1/mavros/state"
     );
+    pnh.param<std::string>(
+        "odom_topic",
+        _odom_topic,
+        "/uav1/mavros/local_position/odom"
+    );
 
     _state_sub = nh.subscribe(
         _state_topic,
         10,
         &VehicleInterface::stateCallback,
+        this
+    );
+    _odom_sub = nh.subscribe(
+        _odom_topic,
+        10,
+        &VehicleInterface::odomCallback,
         this
     );
 }
@@ -44,4 +56,11 @@ std::string VehicleInterface::mode() const
 bool VehicleInterface::hasState() const
 {
     return _has_state;
+}
+
+void VehicleInterface::odomCallback(
+    const nav_msgs::Odometry::ConstPtr& msg)
+{
+    _odom = *msg;
+    _has_odom = true;
 }
