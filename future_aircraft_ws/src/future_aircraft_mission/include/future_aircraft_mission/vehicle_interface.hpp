ad12f9eff26480b321e97bdda276296694f9736e
root <root@PC-202307281902.localdomain>
2026-09-03 18:42:54 +0800

    position

--- future_aircraft_ws/src/future_aircraft_mission/include/future_aircraft_mission/vehicle_interface.hpp
+++ future_aircraft_ws/src/future_aircraft_mission/include/future_aircraft_mission/vehicle_interface.hpp
@@ -2,6 +2,8 @@
 #include <ros/ros.h>
 #include <string>
 #include <mavros_msgs/State.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Point.h>
 
 class VehicleInterface
 {
@@ -13,15 +15,22 @@ public:
     std::string mode() const;
     bool hasState() const;
 
+    geometry_msgs::Point position() const;
+    bool hasOdom() const;
+
 private:
     void stateCallback(const mavros_msgs::State::ConstPtr& msg);
-
+    void odomCallback(const nav_msgs::Odometry::ConstPtr& msg);
 
     ros::Subscriber _state_sub;
+    ros::Subscriber _odom_sub;
 
     mavros_msgs::State _state;
+    nav_msgs::Odometry _odom;
 
     std::string _state_topic;
+    std::string _odom_topic;
 
     bool _has_state;
+    bool _has_odom;
 };
